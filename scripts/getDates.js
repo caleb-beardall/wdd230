c199a34b8e528583d8fff7b3021f5fd6646a97b2
caleb-beardall <calebbeardall@byui.edu>
2025-03-08 17:15:09 -0800

    Completed W01 Activity: Your Course Home Page

---
+++ scripts/getDates.js
@@ -0,0 +1,8 @@
+const cYear = document.getElementById("copyright");
+const mDate = document.getElementById("lastModified");
+
+let copyrightYear = new Date();
+let lastModified = new Date(document.lastModified);
+
+cYear.textContent = `©${copyrightYear.getFullYear()} | Caleb Beardall | Ogden, Utah`;
+mDate.textContent = `Last Modification: ${lastModified.toLocaleString('de-DE')}`;
