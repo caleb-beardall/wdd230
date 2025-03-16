bca6196a6c1b554cdd97d522b5c88cf73398ad11
caleb-beardall <calebbeardall@byui.edu>
2025-03-15 20:00:32 -0700

    Updated W02 Team Activity: Home Page Enhancement

--- scripts/index.js
+++ scripts/index.js
@@ -23,32 +23,24 @@ const darkButton = document.querySelector('#darkMode');
 darkButton.addEventListener('click', () => {
 
     if (darkMode.classList.contains('on')) {
-        /*
-        document.documentElement.style.setProperty('--dark-blue', '#3367d6');
-        document.documentElement.style.setProperty('--med-blue', '#4285f4');
-        document.documentElement.style.setProperty('--light-blue', '#7baaf7');
-        document.documentElement.style.setProperty('--highlight-blue', '#c6dafc');
-        */
 
         document.documentElement.style.setProperty('--title-text-color', 'white');
         document.documentElement.style.setProperty('--body-text-color', 'black');
         document.documentElement.style.setProperty('--background-color', 'white');
         document.documentElement.style.setProperty('--section-color', '#f2f2f2');
         document.documentElement.style.setProperty('--border-shadow-color', 'rgba(0, 0, 0, 0.1)');
+        document.documentElement.style.setProperty('--link-color', '#1e6dae');
+        document.documentElement.style.setProperty('--visited-link-color', '#6926cd');
 
     } else {
-        /*
-        document.documentElement.style.setProperty('--dark-blue', '#3367d6cc');
-        document.documentElement.style.setProperty('--med-blue', '#4285f4cc');
-        document.documentElement.style.setProperty('--light-blue', '#7baaf7cc');
-        document.documentElement.style.setProperty('--highlight-blue', '#c6dafccc');
-        */
 
         document.documentElement.style.setProperty('--title-text-color', 'black');
         document.documentElement.style.setProperty('--body-text-color', 'white');
         document.documentElement.style.setProperty('--background-color', 'black');
         document.documentElement.style.setProperty('--section-color', '#191919');
         document.documentElement.style.setProperty('--border-shadow-color', 'rgba(255, 255, 255, 0.1)');
+        document.documentElement.style.setProperty('--link-color', '#67ace5');
+        document.documentElement.style.setProperty('--visited-link-color', '#b089e9');
     }
 
     darkButton.classList.toggle('on');
