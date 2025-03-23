54f7235b92a826a99bea8cf27f476c188668107c
caleb-beardall <calebbeardall@byui.edu>
2025-03-22 22:33:38 -0700

    Updated W02 Project Milestone

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -13,5 +13,5 @@ const myInfo = document.querySelector('.myInfo');
 let copyrightYear = new Date();
 let lastModified = new Date(document.lastModified);
 
-myInfo.textContent = `©${copyrightYear.getFullYear()} | Caleb Beardall | Last Modification: ${lastModified.toLocaleString('de-DE')}`;
+myInfo.textContent = `©${copyrightYear.getFullYear()} | Caleb Beardall | WDD 230 Project | Last Modification: ${lastModified.toLocaleString('de-DE')}`;
 
