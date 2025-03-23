5d599e505dae96019f8a61b4d36ccd6c38e96897
caleb-beardall <calebbeardall@byui.edu>
2025-03-23 16:42:46 -0700

    Completed W03 Team Activity: Home Page Responsive Images

--- scripts/index.js
+++ scripts/index.js
@@ -28,7 +28,7 @@ darkButton.addEventListener('click', () => {
         document.documentElement.style.setProperty('--body-text-color', 'black');
         document.documentElement.style.setProperty('--background-color', 'white');
         document.documentElement.style.setProperty('--section-color', '#F5F5F5');
-        document.documentElement.style.setProperty('--border-shadow-color', 'rgba(0, 0, 0, 0.1)');
+        document.documentElement.style.setProperty('--border-shadow-color', 'rgba(0, 0, 0, 0.25)');
         document.documentElement.style.setProperty('--link-color', '#1e6dae');
         document.documentElement.style.setProperty('--visited-link-color', '#6926cd');
 
@@ -38,7 +38,7 @@ darkButton.addEventListener('click', () => {
         document.documentElement.style.setProperty('--body-text-color', 'white');
         document.documentElement.style.setProperty('--background-color', 'black');
         document.documentElement.style.setProperty('--section-color', '#212121');
-        document.documentElement.style.setProperty('--border-shadow-color', 'rgba(255, 255, 255, 0.1)');
+        document.documentElement.style.setProperty('--border-shadow-color', 'rgba(255, 255, 255, 0.25)');
         document.documentElement.style.setProperty('--link-color', '#67ace5');
         document.documentElement.style.setProperty('--visited-link-color', '#b089e9');
     }
@@ -46,3 +46,18 @@ darkButton.addEventListener('click', () => {
     darkButton.classList.toggle('on');
 
 });
+
+/* Visits Counter */
+const visitsCounter = document.querySelector('.visits');
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+    visitsCounter.textContent = `Visits Count: ${numVisits + 1}`;
+} else {
+    visitsCounter.textContent = `Welcome to my website!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
