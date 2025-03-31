4dc2ef7197b376480b502cced27991e8915acc3b
caleb-beardall <calebbeardall@byui.edu>
2025-03-30 19:17:16 -0700

    Updated W03 Team Activity: Form Build

--- scripts/form.js
+++ scripts/form.js
@@ -68,10 +68,9 @@ function confirmation() {
 const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("r");
 
-// RANGE event listener
 range.addEventListener('change', displayRatingValue);
 range.addEventListener('input', displayRatingValue);
 
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
-}
+};
