5a6ebc4b2aa98db819805fa7cf2cb4fa7d90b402
caleb-beardall <calebbeardall@byui.edu>
2025-04-10 19:59:57 -0700

    Completed W05 Team Activity: Home Page Weather

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,27 @@
 const baseURL = "https://caleb-beardall.github.io/wdd230/";
 const linksURL = "https://caleb-beardall.github.io/wdd230/data/links.json";
 
+
+const displayLinks = (weeks) => {
+    const ul = document.getElementById('myList');
+
+    weeks.forEach((week) => {
+        const li = document.createElement('li');
+
+        const anchorTags = week.links.map(link => {
+            return `<a href="${link.url}">${link.title}</a>`;
+        });
+
+        li.innerHTML = `${week.week}: ${anchorTags.join(' | ')}`;
+        ul.appendChild(li);
+
+    });
+}
+
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    displayLinks(data.weeks);
 }
 
 getLinks();
