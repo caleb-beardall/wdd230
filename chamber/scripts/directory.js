8401f5988fb2c5b0974b909660dac5ce2f1c6aa7
caleb-beardall <calebbeardall@byui.edu>
2025-04-12 11:25:21 -0700

    Working on W05 Project Milestone - Directory Page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,19 @@
+/* Menu */
+const mainMenu = document.querySelector('#mainMenu');
+const menuButton = document.querySelector('#menuButton');
+
+menuButton.addEventListener('click', () => {
+    mainMenu.classList.toggle('open');
+    menuButton.classList.toggle('open');
+});
+
+/* Membership List */
+const url = ""
+
+/* Footer */
+const myInfo = document.querySelector('.myInfo');
+
+const currentDate = new Date;
+const lastModified = new Date(document.lastModified);
+
+myInfo.textContent = `©${currentDate.getFullYear()} | Caleb Beardall | WDD 230 Project | Last Modification: ${lastModified.toLocaleString('de-DE')}`;
